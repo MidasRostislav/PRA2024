8ce241e1dd21b3e43291f6ac3b387d783989c8a8
MidasRostislav <evklid.lavrentevich@gmail.com>
2023-11-30 14:42:16 +0100

    XML JSON end of lesson

--- src/main/java/model/Employee.java
+++ src/main/java/model/Employee.java
@@ -1,9 +1,16 @@
 package model;
 
+import com.fasterxml.jackson.annotation.*;
+import org.joda.time.DateTime;
+
 import java.util.ArrayList;
 import java.util.List;
 
+@JsonIdentityInfo(generator= ObjectIdGenerators.IntSequenceGenerator.class,
+        property="refId", scope=Employee.class)
 public class Employee {
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss.SSSZ")
+    DateTime birthDate;
 
     private int id;
 
@@ -11,8 +18,10 @@ public class Employee {
 
     private String lastName;
 
+    @JsonProperty("wynagrodzenie")
     private int salary;
 
+    @JsonIgnore
     private int pesel;
 
     Address address;
@@ -85,5 +94,13 @@ public class Employee {
 
     public void setAddress(Address address) {
         this.address = address;
+    }
+
+    public DateTime getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(DateTime birthDate) {
+        this.birthDate = birthDate;
     }
 }
--- src/main/java/pl/edu/amu/pracprog/JacksonSerialization.java
+++ src/main/java/pl/edu/amu/pracprog/JacksonSerialization.java
@@ -2,12 +2,16 @@ package pl.edu.amu.pracprog;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import com.fasterxml.jackson.datatype.joda.JodaModule;
 import model.Employee;
+import org.apache.log4j.EnhancedPatternLayout;
 import org.apache.log4j.Logger;
 
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 
 public class JacksonSerialization {
 
@@ -20,6 +24,12 @@ public class JacksonSerialization {
         //Create objects to serialize
         ModelObjectsCreator objectsCreator = new ModelObjectsCreator();
         Employee employee = objectsCreator.getEmp();
+
+
+        ArrayList<Employee> workers = (ArrayList<Employee>) objectsCreator.getEmployees();
+
+        mapper.writeValue(new File("workers." + fileSuffix), workers);
+
 
         //Serialize to file and string
         mapper.writeValue(new File("result." + fileSuffix), employee);
@@ -44,11 +54,18 @@ public class JacksonSerialization {
 
     public static void deserializeDemo(ObjectMapper mapper, String fileSuffix) throws IOException {
         //Deserialized employee object from employees.* file in resources
-        InputStream employeeIs = JacksonSerialization.class.getClassLoader().
-                getResourceAsStream("employee." + fileSuffix);
+//        InputStream employeeIs = JacksonSerialization.class.getClassLoader().
+//                getResourceAsStream("result." + fileSuffix);
+
+        System.out.println("Deserialization of Workers");
+        ArrayList<Employee> workers = mapper.readValue(new File("workers." + fileSuffix), ArrayList.class);
+        String desWorkers = mapper.writeValueAsString(workers);
+        System.out.println(desWorkers);
+
+        System.out.println();
 
         //Read value - set class type of serialization
-        Employee deserializedEmployee = mapper.readValue(employeeIs, Employee.class);
+        Employee deserializedEmployee = mapper.readValue(new File("result." + fileSuffix), Employee.class);
 
         //Give eployee big salary
         deserializedEmployee.setSalary(100000);
@@ -62,8 +79,15 @@ public class JacksonSerialization {
     public static void main(String[] args) throws IOException {
 
         ObjectMapper jsonMapper = new ObjectMapper();
-        serializeDemo(jsonMapper, "json");
+        jsonMapper.registerModule(new JodaModule());
+        //serializeDemo(jsonMapper, "json");
         deserializeDemo(jsonMapper, "json");
+
+        ObjectMapper xmlMapper = new XmlMapper();
+        xmlMapper.registerModule(new JodaModule());
+        //serializeDemo(xmlMapper, "xml");
+        //deserializeDemo(xmlMapper, "xml ");
+
 
     }
 }
